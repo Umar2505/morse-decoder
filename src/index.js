a92762668c5a642274adeb9ac2fcd092b8051912
Umar <rustamovumar0@gmail.com>
2022-02-04 23:11:21 +0500

    fixed

--- src/index.js
+++ src/index.js
@@ -35,10 +35,35 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    var keys=Object.keys(MORSE_TABLE)
+    var elements=Object.values(MORSE_TABLE)
+    var str = expr.match(/.{10}/g)
+    var result = []
+    for(var i=0;i<=str.length-1;i++) {
+        var res= str[i].match(/.{1}./g)
+        for(var j=0;j<=res.length-1;j++) {
+            if(res[j]=="10") {
+                res[j]="."
+            }else if(res[j]=="11") {
+                res[j]="-"
+            }else if(res[j]=="00") {
+                res[j]=""
+            }
+        }
+        result.push(res.join(""))
+    }
+    for(var i=0;i<=result.length-1;i++) {
+        for(var j=0;j<=keys.length-1;j++) {
+            if(result[i]==keys[j]) {
+                result[i]=elements[j]
+            }
+        }
+    }
+    return result.join("")
 }
 
 module.exports = {
